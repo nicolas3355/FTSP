4040c72abc473aba2df7a69644dffede5533bda9
Magtheridon96 <calculabaydoun@hotmail.com>
2016-02-16 23:21:21 +0200

    lalalala

---
+++ Server/src/TestLib.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class TestLib {
+
+	public static int sum(int x, int y) {
+		return x + y;
+	}
+
+	public static long product(long x, long y) {
+		return x * y;
+	}
+	
+	public static sqrt(double x) {
+		double s = x/2;
+		for (int i = 0; i < 50; ++i) {
+			s = 0.5*(s + x/s);
+		}
+		return s;
+	}
+
+	public static void doNothing() {
+		return;
+	}
+}
